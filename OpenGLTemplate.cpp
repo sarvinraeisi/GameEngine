77664c5d67a77018882bbfc70b3cfc1c50ca7f67
unknown <raeisisarvin@gmail.com>
2024-03-08 13:22:56 -0500

    Initial commit

--- OpenGLTemplate.cpp
+++ OpenGLTemplate.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include "shaderinit.h"
 
 #include <glm/glm.hpp>
@@ -8,46 +9,61 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <iostream>
-#include <cstdlib>
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void init(void);
-void render(Shader shader);
+void render();
+void tranformations(Shader& ourShader);
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+void texture1Rendering();
+void texture2Rendering();
+void applyTexture(unsigned int& texture);
 
-void tranformations(Shader&, bool, int);
-
+// variables
 const unsigned int screen_width = 1200;
 const unsigned int screen_height = 800;
-
-glm::mat4 transformationsMatrix[4]{};
-
-unsigned int selectedRect{};
-bool rotating = false;
-bool scaling = false;
-bool moveToCenter = false;
-float angle = 0.0f;
-GLuint NumVertices = 6;
+const GLuint NumVertices = 36;
 GLuint VBO;
 GLuint VAO;
 GLuint EBO;
 
+unsigned int texture1, texture2;
+unsigned int texture;
+int width, height, nrChannels;
+// camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+float lastX = screen_width / 2.0f;
+float lastY = screen_height / 2.0f;
+bool firstMouse = true;
+float yaw = -90.0f; // yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right
+float pitch = 0.0f;
+float mouseSensitivity = 0.1f;
+
 int main()
 {
+    // glfw: initialize and configure
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-#ifdef __APPLE__
+#ifdef _APPLE_
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
     // glfw window creation
     // --------------------
     GLFWwindow* window;
-    window = glfwCreateWindow(screen_width, screen_height, "OpenGLTwoTrianglesExternalShaderFiles", NULL, NULL);
+    window = glfwCreateWindow(screen_width, screen_height, "OpenGLCameraAuto", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -65,233 +81,320 @@ int main()
         return -1;
     }
 
-
+    // Enable Depth Testing
+    glEnable(GL_DEPTH_TEST);
+    // build and compile our shader program
     Shader ourShader("shader.vs", "shader.fs"); // you can name your shader files however you like
 
     init();
+    // primitives spawning locations
+    glm::vec3 primPositions[] = {
+        glm::vec3(0.0f,  0.0f,  0.0f),
+        glm::vec3(2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f,  2.0f, -2.5f),
+        glm::vec3(1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
+    applyTexture(texture);
+
+    texture1Rendering();
+    // texture 2
+    // ---------
+    texture2Rendering();
+
+    tranformations(ourShader);
+
+    ourShader.use(); // activate the shader
+
+    // pass the uniform variables
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    ourShader.setInt("texture2", 1);
+
+    // camera/view transformation
+    glm::mat4 view = glm::mat4(1.0f); // view matrix initialization
+    // setting the radius variable 
+    float radius = 10.0f;
 
     // render loop
     while (!glfwWindowShouldClose(window))
     {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
         // input
-        // -----
         processInput(window);
-        //std::cout << "selected: " << selectedRect << std::endl;
-       
-        render(ourShader);
-        
-       
+
+        // Inside main() before the render loop, set the mouse callback
+        glfwSetCursorPosCallback(window, mouse_callback);
+
+        // To hide the mouse cursor and capture it, add this line as well
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+        // render the background
+        render();
+        // Shader instance use
+        ourShader.use();
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        // add the view matrix
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        ourShader.setMat4("view", view);
+        // render boxes
+        glBindVertexArray(VAO);
+
+        for (int i = 0; i < 1; i++) {
+            // model matrix and primitives spawning
+            glm::mat4 model = glm::mat4(1.0f); // initialize the model matrix
+            model = glm::translate(model, primPositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 24);
+        }
+        // glfw: swap buffers
         glfwSwapBuffers(window);
+        // gldw: poll for events
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    //glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
 
-void tranformations(Shader& ourShader, bool isSelected, int rectIdx)
+void tranformations(Shader& ourShader)
 {
-    glm::vec3 center;
-    glm::vec3 screenCenter = glm::vec3(0.0f, 0.0f, 0.0f);
-
-
-    // array of centers for the rectangles
-    glm::vec3 centers[] = {
-        glm::vec3(0.8f, 0.8f, 0.0f),   
-        glm::vec3(0.8f, -0.8f, 0.0f),  
-        glm::vec3(-0.8f, -0.8f, 0.0f), 
-        glm::vec3(-0.8f, 0.8f, 0.0f)   
-    };
-    center = centers[rectIdx];
-
-    if (isSelected)
-    {
-        angle += 45.0f;
-        transformationsMatrix[rectIdx] = glm::translate(transformationsMatrix[rectIdx], center); 
-        transformationsMatrix[rectIdx] = glm::rotate(transformationsMatrix[rectIdx], glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
-        transformationsMatrix[rectIdx] = glm::translate(transformationsMatrix[rectIdx], -center); 
-    }
-
-    glm::mat4 transform = transformationsMatrix[rectIdx];
-
-    if (scaling && isSelected)
-    {
-        // current scale
-        glm::vec3 scale = glm::vec3(glm::length(transform[0]), glm::length(transform[1]), glm::length(transform[2]));
-
-       
-        if (glm::length(scale - glm::vec3(2.5f, 2.5f, 1.0f)) > 0.01f)
-        {
-            // Scale the rectangle by a factor of 2.5
-            transform = glm::translate(transform, center); 
-            transform = glm::scale(transform, glm::vec3(2.5f, 2.5f, 1.0f) / scale);
-            transform = glm::translate(transform, -center); 
-
-        }
-        scaling = false; 
-    }
-
-    transformationsMatrix[rectIdx] = transform;
-
-    if (moveToCenter && isSelected)
-    {
-        glm::vec4 currentCenter = transformationsMatrix[rectIdx] * glm::vec4(center, 1.0f);
-
-        glm::vec3 translation = screenCenter - glm::vec3(currentCenter);
-
-        if (glm::length(translation) > 0.01f)
-        {
-            glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), translation);
-
-            transformationsMatrix[rectIdx] = translationMatrix * transformationsMatrix[rectIdx];
-        }
-        moveToCenter = false;
-    }
-
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)screen_width / (float)screen_height, 0.1f, 100.0f);
     ourShader.use();
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transformationsMatrix[rectIdx]));
+    ourShader.setMat4("projection", projection);
 }
 
-void render(Shader shader)
+void render()
 {
-    static const float black[] = { 1.0f, 0.0f, 0.0f, 0.0f };
-    glClearBufferfv(GL_COLOR, 0, black);
-    glClear(GL_COLOR_BUFFER_BIT);  
-    glBindVertexArray(VAO); 
-    // Draw each rectangle
-    for (int i = 0; i < 4; i++)
-    {
-        if (rotating && i == selectedRect) 
-        {
-            tranformations(shader, true, i);
-        }
-        else
-        {
-            tranformations(shader, false, i); 
-        }
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(static_cast<unsigned long long>(i) * 6 * sizeof(unsigned int)));
-    }
+    glClearColor(0.5f, 0.0f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void init(void)
 {
     float vertices[] = {
-      // Rectangle 1 
-      0.95f,  0.95f, 0.0f, 1.0f, 0.0f, 0.0f,
-      0.65f,  0.95f, 0.0f, 1.0f, 0.0f, 0.0f,
-      0.65f,  0.65f, 0.0f, 1.0f, 0.0f, 0.0f,
-      0.95f,  0.65, 0.0f, 1.0f, 0.0f, 0.0f,
+        // Rectangle in Top-Left Corner - Red
+         -1.0f,  1.0f, 0.0f,  1.0f, 0.0f, 0.0f,
+         -0.8f,  1.0f, 0.0f,  1.0f, 0.0f, 0.0f,
+         -1.0f,  0.8f, 0.0f,  1.0f, 0.0f, 0.0f,
+         -0.8f,  1.0f, 0.0f,  1.0f, 0.0f, 0.0f,
+         -1.0f,  0.8f, 0.0f,  1.0f, 0.0f, 0.0f,
+         -0.8f,  0.8f, 0.0f,  1.0f, 0.0f, 0.0f,
 
-      // Rectangle 2
-      0.95f, -0.95f, 0.0f, 0.0f, 1.0f, 0.0f,
-      0.65f, -0.95f, 0.0f, 0.0f, 1.0f, 0.0f,
-      0.65f, -0.65f, 0.0f, 0.0f, 1.0f, 0.0f,
-      0.95f, -0.65f, 0.0f, 0.0f, 1.0f, 0.0f,
+         // Rectangle 2 - Bottom-right corner
+        0.8f, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
+        0.8f, -0.8f, 0.0f,  0.0f, 1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,  0.0f, 1.0f, 0.0f,
+        0.8f, -0.8f, 0.0f,  0.0f, 1.0f, 0.0f,
+        1.0f, -0.8f, 0.0f,  0.0f, 1.0f, 0.0f,
 
-      // Rectangle 3 
-      -0.95f, -0.95f, 0.0f, 0.5f, 0.0f, 1.0f,
-      -0.65f, -0.95f, 0.0f, 0.5f, 0.0f, 1.0f,
-      -0.65f, -0.65f, 0.0f, 0.5f, 0.0f, 1.0f,
-      -0.95f, -0.65f, 0.0f, 0.5f, 0.0f, 1.0f,
+        // Rectangle 3 - Top-right corner
+        0.8f,  0.8f, 0.0f,  0.0f, 0.0f, 1.0f,
+        1.0f,  0.8f, 0.0f,  0.0f, 0.0f, 1.0f,
+        0.8f,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+        1.0f,  0.8f, 0.0f,  0.0f, 0.0f, 1.0f,
+        0.8f,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+        1.0f,  1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
 
-      // Rectangle 4
-      -0.95f,  0.95f, 0.0f, 1.0f, 0.8f, 0.0f,
-      -0.65f,  0.95f, 0.0f, 1.0f, 0.8f, 0.0f,
-      -0.65f,  0.65f, 0.0f, 1.0f, 0.8f, 0.0f,
-      -0.95f,  0.65f, 0.0f, 1.0f, 0.8f, 0.0f,
-
+        // Rectangle 4 - Top-left corner
+        -1.0f, -1.0f, 0.0f,  1.0f, 1.0f, 0.0f, // Bottom Left
+        -0.8f, -1.0f, 0.0f,  1.0f, 1.0f, 0.0f, // Bottom Right
+        -1.0f, -0.8f, 0.0f,  1.0f, 1.0f, 0.0f, // Top Left
+        -0.8f, -1.0f, 0.0f,  1.0f, 1.0f, 0.0f, // Bottom Right
+        -1.0f, -0.8f, 0.0f,  1.0f, 1.0f, 0.0f, // Top Left
+        -0.8f, -0.8f, 0.0f,  1.0f, 1.0f, 0.0f, // Top Rig
     };
 
-    unsigned int indices[] = {
-        0, 1, 2,  
-        2, 3, 0,  
 
-        4, 5, 6,  
-        6, 7, 4,   
 
-        8, 9, 10,  
-        10, 11, 8, 
-
-        12, 13, 14, 
-        14, 15, 12 
-    };
-
-    NumVertices = sizeof(indices) / sizeof(indices[0]);
-    
-    for (int i = 0; i < 4; i++)
-    {
-        transformationsMatrix[i] = glm::mat4(1.0f);
-    }
-
+    // Generate and bind VAO and VBO as before
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    
+    // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-   
+    // color attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    //// position attribute information
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    //glEnableVertexAttribArray(0);
+    //// color attribute information
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
+    //// texture coord attribute information
+    //glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    //glEnableVertexAttribArray(2);
+
+    // Unbind VBO and VAO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
 
-// user input
+// glfw: user input
 void processInput(GLFWwindow* window)
 {
-    if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) 
-    {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
-        std::cout << "user closed the window by pressing M" << std::endl;
-    }
-
-    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
-    {
-        rotating = true;
-        float newRand = static_cast<int>(rand() % 4);
-     
-        selectedRect = newRand;
-    }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    {
-        scaling = true;
-       
-    }
+    // increate the camera speed using the deltaTime
+    float cameraSpeed = 3 * deltaTime;
+    // forward movement
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    {
-        moveToCenter = true;
-
-    }
-    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-    {
-        rotating = false;
-        scaling = false;
-        moveToCenter = false;
-        angle = 0.0f;
-
-        for (int i = 0; i < 4; i++)
-        {
-            transformationsMatrix[i] = glm::mat4(1.0f);
-        }
-    }
+        cameraPos += cameraSpeed * cameraFront;
+    // backward movement
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    // left movement
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    // right movement
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 // glfw: viewport to window adjustment
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+// Mouse callback function
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    xoffset *= mouseSensitivity;
+    yoffset *= mouseSensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // Make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+}
+
+void texture2Rendering()
+{
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // texture wrapping params
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set wrapping to GL_REPEAR
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // filtering params
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    // load image (smilie.png) and create the texture 
+    unsigned char* data1 = stbi_load("assets/smilie.png", &width, &height, &nrChannels, 0);
+    if (data1)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data1);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data1);
+}
+
+void texture1Rendering()
+{
+    // texture 1
+    // ---------
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image (smilie.png) and create the texture 
+    unsigned char* data = stbi_load("assets/mybox.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+}
+void applyTexture(unsigned int& texture)
+{
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // the texture object is applied with all the texture operations
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set GL_REPEAT as the wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image (mybox.png) and create the texture 
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("assets/mybox.png", &width, &height, &nrChannels, 0);
+    // Generate mipmaps
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
 }
